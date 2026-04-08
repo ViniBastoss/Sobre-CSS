cb686a34bada65a188eb68c8046d610cbe541f6b
Vinicius <vinibastos31@gmail.com>
2026-04-08 20:14:19 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function varTesteDeRotina(){
-    var num1 = 1
-    var num1 = 2
+function verificaAprovacao(){
+    let nota = document.getElementById("nota").value;
+    
+    if(nota >= 7){
+        //document.getElementById("nota").value;
+        document.getElementById("resultado").innerHTML = "Aluno(a) aprovado!"
+        alert("Aluno(a) aprovado!")
 
-    console.log() 
+    }
+        else{
+           document.getElementById("resultado").innerHTML = "Aluno(a) reprovado!"
+           alert("Aluno(a) reprovado...")
+        }
+    
+
+    console.log(nota);
 }
+// Dar preferência para o uso de let e cont.
+// Evite o uso do var.
+
