bf3bf424abc7e08550ef1cb0d37508e9962cda78
ViniBastoss <vinibastos31@gmail.com>
2026-05-01 00:36:52 -0300

    att

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const celulas = document.querySelectorAll(".celula");
 
 let vezDoX =true;
+
+document.getElementById("botaoReiniciar").addEventListener("click",iniciarJogo);
+
+function iniciarJogo(){
+celulas.forEach(celula =>{
+    celula.textContent ="";
+    celula.addEventListener("click",tratarClick, {once: true});
+})
+}
+
+function tratarClick(evento){
+
+    evento.target.textContent = vezDoX ? "X" : "O";
+    vezDoX = !vezDoX;
+}
+
+
+iniciarJogo()
